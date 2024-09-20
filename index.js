7178e7709b2fae23943658e443126eb0dffc40b7
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-09-20 13:38:41 +1000

    completed

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet) {
+  if (feet > 2500) {
+    return 'No can do.';
+  } else if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 400 && feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+function ternaryCheckCity(city){
+  if(city=='NYC'){
+  return "Ok, sounds good."
+  }
+  else if(city!=="NYC"){
+    return "No go."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if(tip == 'generous')
+  {
+    return  'Thank you so much.'
+  }
+  else if(tip == 'not as generous'){
+   return 'Thank you.'
 
-function switchOnCharmFromTip(){
+  }
+  else {
+    return 'Bye.'
+
+  }
   // Write your code here!
 }
+scuberGreetingForFeet(3455)
+ternaryCheckCity("NYC")
+switchOnCharmFromTip('generous')
